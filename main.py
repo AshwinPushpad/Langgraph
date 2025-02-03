f90bad856f1eac96f2da9f6151cb663b984b2ca3
Ashwin Pushpad <ashwin006pushpad@gmail.com>
2025-02-03 21:45:36 +0530

    Added Langgraph Chatbot

--- main.py
+++ main.py
@@ -12,6 +12,20 @@ os.environ["OPENAI_API_KEY"]=OPENAI_API_KEY
 # Initialize OpenAI Chat Model
 llm = ChatOpenAI(model="gpt-4o")
 
+# Only using OpenAI
+# from openai import OpenAI
+# client = OpenAI()
+#
+# completion = client.chat.completions.create(
+#   model="gpt-4o",
+#   messages=[
+#     {"role": "developer", "content": "You are a helpful assistant."},
+#     {"role": "user", "content": "Hello!"}
+#   ]
+# )
+#
+# print(completion.choices[0].message.content)
+
 while True:
     print(llm.invoke("Hey there!").content)
     prompt =input("Type Here: ")
